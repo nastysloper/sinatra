fd743c416c2052b1e118abc5e20380c5c3b4a528
Richard Vogt <richvogtjr@gmail.com>
2013-06-24 18:52:33 -0500

    initial commit

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'slim'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  @title = 'HONK!'
+  slim :home
+end
+
+get '/about' do
+  @title = 'about'
+  slim :about
+end
+
+get '/contact' do
+  @title = 'contact'
+  slim :contact
+end
+
+get '/instance' do
+  @title= 'instance'
+  slim :show
+end
+
+not_found do
+  slim :not_found
+end
